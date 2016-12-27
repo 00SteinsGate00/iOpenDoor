8db1e8cb9893d62d922d93d27281725a75b550a8
Jan Holub <Jan-Holub@gmx.net>
2016-12-28 00:30:17 +0100

    Implemented a function to fetch the door status from the OpenDoor API and set the UI accordingly

--- iOpenDoor/ViewController.swift
+++ iOpenDoor/ViewController.swift
@@ -10,27 +10,93 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK vars
     @IBOutlet weak var lockSymbol: UIImageView!
     @IBOutlet weak var timeStamp: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.updateStatus()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
+    // calls the OpenDoor API and sets the UI to open or closed accordingly
+    func updateStatus() {
+        
+        // request to the OpenDoor API
+        let urlrequest = URLRequest(url: URL(string: "https://www.fachschaft.cs.uni-kl.de/opendoor.json")!)
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        // task to handle the request and set the status accordingly
+        let task = session.dataTask(with: urlrequest, completionHandler: {(data, response, error) in
+            // check for error
+            if(error != nil){
+                print("Error calling OpenDoor")
+
+                return
+            }
+            // no error
+            else{
+                do{
+                    
+                    // convert the JSON object
+                    let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+                    
+                    // retrieve the 'opendoor' value as a bool
+                    if let status = json["opendoor"] as? Bool {
+                        
+                        
+                        // the following code will update the UI and should thus not run async
+                        DispatchQueue.main.async {
+                            // open
+                            if(status){
+                                self.doorIsOpen()
+                            }
+                            // closed
+                            else{
+                                self.doorIsClosed()
+                            }
+                            
+                        }
+                        
+                        
+                    }
+                    // 'opendoor' not found in response JSON
+                    else{
+                        print("Error getting opendoor from JSON")
+                        return
+                    }
+                    
+                    
+                }
+                // error converting the JSON object
+                catch {
+                    print ("Error trying to parse JSON")
+                    return
+                }
+                
+                
+            }
+        })
+        
+        // perform the task
+        task.resume()
+        
+    }
     
-    
+    // updates the UI to open
     func doorIsOpen() {
         self.view.backgroundColor = UIColor(red: 46/255.0, green: 204/255.0, blue: 113/255.0, alpha: 1.0)
         self.lockSymbol.image = UIImage(named: "LockOpened")
     }
     
+    // updates the UI to closed
     func doorIsClosed() {
         self.view.backgroundColor = UIColor(red: 252/255.0, green: 96/255.0, blue: 92/255.0, alpha: 1.0)
         self.lockSymbol.image = UIImage(named: "LockClosed")
